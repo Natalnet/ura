1622657b3ae6c515cda77b03eef528bc742ebed7
Pedro Henrique <pedrotrab2132@gmail.com>
2020-02-01 05:28:52 -0300

    Adiconar burger menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components'
 import Routes from './routes';
+import { GlobalStyles } from './Global'
+import { theme } from './Theme'
 
 import Header from './components/header';
 import Footer from './components/footer';
@@ -8,11 +11,16 @@ import Footer from './components/footer';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes />
-      <Footer />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <BrowserRouter>
+          <Header />
+          <Routes />
+          <Footer />
+        </BrowserRouter>
+      </>
+    </ThemeProvider>
   );
 }
 
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,37 +1,60 @@
-import React, { Component } from 'react' 
-import { AwesomeButton } from 'react-awesome-button';
+import React, {  useState, useRef } from 'react'
 import { Link } from "react-router-dom";
+import { ThemeProvider } from 'styled-components';
+
+// Components
+import { useOnClickOutside } from '../../hooks';
+import { theme } from '../../Theme';
+import { GlobalStyles } from '../../Global';
+import Burger from '../../components/Burger';
+import Menu from '../../components/Menu';
+import { AwesomeButton } from 'react-awesome-button';
+
+// Styled
+import './header.scss'
+
 // eslint-disable-next-line no-unused-vars
 import Styless from './react-awesome-button-master/src/styles/themes/theme-blue/index';
 
-import './header.scss'
-
+// Midia
 import logo from '../../img/logo-ura.png'
 
-export default class Header extends Component {
-    render() {
+export default function Header()  {
+        const [open, setOpen] = useState(false);
+        const node = useRef();
+        useOnClickOutside(node, () => setOpen(false));
         return (
-                <header className="main-header">
+                <ThemeProvider theme={theme}>
+                    <>
+                        <GlobalStyles />
+                        <header className="main-header">
 
-                    <div className="header-content">
+                            <div className="header-content">
 
-                        <div className="header-logo">
-                            <Link to="/ura/" className="a-logo"><img src={logo} className="img-logo" alt=""></img></Link>
-                        </div>
+                            <div ref={node}>
+                                <Burger open={open} setOpen={setOpen} />
+                                <Menu open={open} setOpen={setOpen} />
+                            </div>
 
-                        <nav className="main-nav">
-                            <ul className="ul-nav">
-                                <li><Link to="/ura/quem-somos"><AwesomeButton type="primary" size="medium"  >Quem Somos</AwesomeButton></Link></li>
-                                <li><Link to="/ura/tutoriais"><AwesomeButton type="primary" size="medium" >Tutoriais</AwesomeButton></Link></li>
-                                <li><Link to="/ura/noticiais"><AwesomeButton type="primary" size="medium" >Notíciais</AwesomeButton></Link></li>
-                                <li><Link to="/ura/metologia"><AwesomeButton type="primary" size="medium" >Metologia</AwesomeButton></Link></li>
-                                <li><Link to="/ura/contato"><AwesomeButton type="primary" size="medium" >Contato</AwesomeButton></Link></li>
-                            </ul>
-                        </nav>
+                                <div className="header-logo">
+                                    <Link to="/ura/" className="a-logo"><img src={logo} className="img-logo" alt=""></img></Link>
+                                </div>
 
-                    </div>
+                                <nav className="main-nav">
+                                    <ul className="ul-nav">
+                                        <li><Link to="/ura/quem-somos"><AwesomeButton type="primary" size="medium"  >Quem Somos</AwesomeButton></Link></li>
+                                        <li><Link to="/ura/tutoriais"><AwesomeButton type="primary" size="medium" >Tutoriais</AwesomeButton></Link></li>
+                                        <li><Link to="/ura/noticiais"><AwesomeButton type="primary" size="medium" >Notíciais</AwesomeButton></Link></li>
+                                        <li><Link to="/ura/metologia"><AwesomeButton type="primary" size="medium" >Metologia</AwesomeButton></Link></li>
+                                        <li><Link to="/ura/contato"><AwesomeButton type="primary" size="medium" >Contato</AwesomeButton></Link></li>
+                                    </ul>
+                                </nav>
 
-                </header>    
+                            </div>
+
+                        </header>
+                    </>    
+                </ThemeProvider>
         )
-    }
+    
 }
