dea1f28da0dbc522a0c98a534335fbf0722894fb
Pedro Henrique <pedrotrab2132@gmail.com>
2020-02-17 15:09:56 -0300

    justify between

--- src/components/Menu/Menu.styled.js
+++ src/components/Menu/Menu.styled.js
@@ -12,9 +12,9 @@ export const StyledMenu = styled.nav`
 
   display: flex;
   flex-direction: column;
-  justify-content: space-evenly;
+  justify-content: space-between;
   background: ${({ theme }) => theme.primaryLight};
-  height: 100vh;
+  max-height: 100vh;
   text-align: left;
   padding: 2rem;
   position: absolute;
