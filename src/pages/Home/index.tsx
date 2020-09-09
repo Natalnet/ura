d2ef39e1591f609fc75aacf0ec17a9b5b7afa0fb
pedroksty <pedrotrab2132@gmail.com>
2020-09-09 03:09:45 -0300

    Animate bottom EnterpriseAbout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useCallback } from "react";
+import React, { useRef, useCallback, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import { FiInstagram, FiFacebook, FiMail } from "react-icons/fi";
@@ -87,6 +87,22 @@ const itemAnimationRightX = {
 
 const Home: React.FC = () => {
   const linkButtonRef = useRef<HTMLAnchorElement>(null);
+  const [lastYPosition, setLatYPosition] = useState(0);
+  const [scroolAnimate, setScroolAnimate] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      setLatYPosition(window.scrollY);
+      if (window.scrollY >= 255) {
+        setScroolAnimate(true);
+      }
+    }
+    window.addEventListener("scroll", handleScroll, false);
+
+    return () => {
+      window.removeEventListener("scrool", handleScroll, false);
+    };
+  }, [lastYPosition]);
 
   const handleMaterial = useCallback(() => {
     linkButtonRef.current?.click();
@@ -162,27 +178,81 @@ const Home: React.FC = () => {
             de paradigmas.
           </p>
         </Card>
-        <Card variants={itemAnimationUpY} initial="hidden" animate="visible">
-          <strong>Posts frequentes</strong>
-          <img src={studyImg} alt="Lendo um livro" />
-          <p>
-            Encontre mais de{" "}
-            <strong>
-              <CountUp start={0} end={23} duration={5} />{" "}
-            </strong>
-            artigos sobré robótica
-          </p>
-        </Card>
-        <Card variants={itemAnimationUpY} initial="hidden" animate="visible">
-          <strong>Treinamentos online</strong>
-          <img src={trainingImg} alt="Felicidade" />
-          <p>Se especialize na sua aŕea com nosso material</p>
-        </Card>
-        <Card variants={itemAnimationUpY} initial="hidden" animate="visible">
-          <strong>Empresas do mercado</strong>
-          <img src={enterpriseLogo} alt="Felicidade" />
-          <p>Aqui você pode buscar vagas em startup's ou empresas do mercado</p>
-        </Card>
+        {scroolAnimate && (
+          <>
+            <Card
+              variants={{
+                hidden: { x: 0, y: 100, opacity: 0 },
+                visible: {
+                  x: 0,
+                  y: 0,
+                  opacity: 1,
+                  transition: {
+                    delay: 0.3,
+                    when: "beforeChildren",
+                    staggerChildren: 0.1,
+                  },
+                },
+              }}
+              initial="hidden"
+              animate="visible"
+            >
+              <strong>Posts frequentes</strong>
+              <img src={studyImg} alt="Lendo um livro" />
+              <p>
+                Encontre mais de{" "}
+                <strong>
+                  <CountUp start={0} end={23} duration={5} />{" "}
+                </strong>
+                artigos sobré robótica
+              </p>
+            </Card>
+            <Card
+              variants={{
+                hidden: { x: 0, y: 100, opacity: 0 },
+                visible: {
+                  x: 0,
+                  y: 0,
+                  opacity: 1,
+                  transition: {
+                    delay: 0.6,
+                    when: "beforeChildren",
+                    staggerChildren: 0.1,
+                  },
+                },
+              }}
+              initial="hidden"
+              animate="visible"
+            >
+              <strong>Treinamentos online</strong>
+              <img src={trainingImg} alt="Felicidade" />
+              <p>Se especialize na sua aŕea com nosso material</p>
+            </Card>
+            <Card
+              variants={{
+                hidden: { x: 0, y: 100, opacity: 0 },
+                visible: {
+                  x: 0,
+                  y: 0,
+                  opacity: 1,
+                  transition: {
+                    delay: 0.9,
+                    when: "beforeChildren",
+                    staggerChildren: 0.1,
+                  },
+                },
+              }}
+              initial="hidden"
+              animate="visible"
+            >
+              <strong>Empresas do mercado</strong>
+              <img src={enterpriseLogo} alt="Felicidade" />
+              <p>
+                Aqui você pode buscar vagas em startup's ou empresas do mercado
+              </p>
+            </Card>
+          </>
+        )}
       </EnterpriseInfo>
 
       <EnterpriseAbout>
